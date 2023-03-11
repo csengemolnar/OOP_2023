673f2595c4471134f1b812130446edf6aaaf6359
csengemolnar <csengemolnar001@gmail.com>
2023-03-11 20:37:26 +0100

    Legelso commit

---
+++ OEP_Beadando1/OEP_Beadando1/Complex.cs
@@ -0,0 +1,86 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Security.Cryptography.X509Certificates;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace OEP_Beadando1
+{
+     class Complex
+    {
+        private double x { get; set; }
+        private double y { get; set; }
+
+        public Complex(double a=0, double b=0)
+        {
+           
+            this.x = a;
+            this.y = b;
+
+        }
+
+        public override string ToString()
+        {
+            if (y < 0)
+            {
+                return $"{x}{y}i";
+            }
+            return $"{x}+{y}i";
+
+
+        }
+
+        static public Complex operator+(Complex a , Complex b)
+        {
+            
+            double X = a.x + b.x;
+            double Y = a.y + b.y;
+            Complex c = new Complex(X, Y);
+
+            return c;
+
+
+        }
+
+        static public Complex operator -(Complex a, Complex b)
+        {
+            
+            double X = a.x - b.x;
+            double Y = a.y - b.y;
+            Complex c = new Complex(X, Y);
+
+            return c;
+
+
+        }
+        static public Complex operator *(Complex a, Complex b)
+        {
+            
+            double X = a.x * b.x - a.y * b.y;
+            double Y = a.x * b.y + a.y * b.x;
+            Complex c = new Complex(X, Y);
+
+
+            return c;
+
+
+        }
+        static public Complex operator /(Complex a, Complex b)
+        {
+            
+            if (b.x == 0 && b.y == 0)
+            {
+                throw new DivideByZeroException();
+            }
+
+            double X = (a.x * b.x + a.y * b.y) / (Math.Pow(b.x,2)+ Math.Pow(b.y, 2));
+            double Y = (a.y * b.x + a.x * b.y) / (Math.Pow(b.x, 2) + Math.Pow(b.y, 2));
+            Complex c = new Complex(X,Y);
+            return c;
+
+
+        }
+
+    }
+}
---
+++ OEP_Beadando1/OEP_Beadando1/Program.cs
@@ -0,0 +1,23 @@
+﻿namespace OEP_Beadando1
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            
+            Complex complex1=new Complex();
+            Complex complex2=new Complex(5,7);
+            Complex complex3=new Complex(6.74,6);
+            Complex complex4=new Complex(7.9953,8);
+
+            Console.WriteLine(complex1 + complex2);
+            Console.WriteLine(complex3 - complex4);
+            Console.WriteLine(complex4 * complex2);
+            Console.WriteLine(complex4 / complex2);
+
+
+
+
+        }
+    }
+}
