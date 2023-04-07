c3dfc80c71d54593669388a86a007d820e239167
csengemolnar <csengemolnar001@gmail.com>
2023-04-07 20:56:09 +0200

    2. kisbeadandó
    OEP 2. beadandó

---
+++ OEPBeadando2/OEPBeadando2/Program.cs
@@ -0,0 +1,64 @@
+﻿using System.Globalization;
+using TextFile;
+
+namespace OEPBeadando2
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            try
+            {
+                Thread.CurrentThread.CurrentCulture = CultureInfo.InvariantCulture;
+
+                TextFileReader reader = new TextFileReader("input.txt");
+
+                double s = 0.0;
+                int db = 0;
+                while (reader.ReadInt(out int e) && e >= 0)
+                {
+                    s = s + e;
+                    db += 1;
+
+                }
+                double a = s / db;
+
+                Console.WriteLine($"Elso fagypont alatti erteket megelozo napok homersekleteinek atlaga: {a} \n");
+
+                bool l = true;
+                double min = 0.0;
+                while (reader.ReadInt(out int e))
+                {
+                    l = l && e < 0;
+
+                    if (min > e)
+                    {
+                        min = e;
+                    }
+
+                }
+
+                if (l == true)
+                {
+                    Console.WriteLine("Fagypont alatt maradt a homerseklet!");
+                }
+                else
+                {
+                    Console.WriteLine("Nem maradt fagypont alatt a homerseklet!");
+                }
+
+                Console.WriteLine($"A legalacsonyabb homerseklet: {min} \n");
+
+
+
+
+
+            }
+            catch (Exception h)
+            {
+
+                Console.WriteLine($"Hiba: {h.Message}"); ;
+            }
+        }
+    }
+}
